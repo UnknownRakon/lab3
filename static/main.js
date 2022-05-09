0a5db4a1211052561fd1807f503f5a0ef3e3404b
UnknownRakon <gorenfeo@gmail.com>
2022-05-09 16:32:54 +0300

    first commit

---
+++ static/main.js
@@ -0,0 +1,153 @@
+// Функция priority позволяет получить 
+// значение приоритета для оператора.
+// Возможные операторы: +, -, *, /.
+
+function priority(operation) {
+    if (operation == '+' || operation == '-') {
+        return 1;
+    } else if (operation != '^') {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+// Проверка, является ли строка str числом.
+function isNumeric(str) {
+    return /^\d+(.\d+){0,1}$/.test(str);
+}
+
+// Проверка, является ли строка str цифрой.
+function isDigit(str) {
+    return /^\d{1}$/.test(str);
+}
+
+// Проверка, является ли строка str оператором.
+function isOperation(str) {
+    return /^[\+\-\*\/\^]{1}$/.test(str);
+}
+
+// Функция tokenize принимает один аргумент -- строку
+// с арифметическим выражением и делит его на токены 
+// (числа, операторы, скобки). Возвращаемое значение --
+// массив токенов.
+
+function tokenize(str) {
+    let tokens = [];
+    let lastNumber = '';
+    for (char of str) {
+        if (isDigit(char) || char == '.') {
+            lastNumber += char;
+        } else {
+            if (lastNumber.length > 0) {
+                tokens.push(lastNumber);
+                lastNumber = '';
+            }
+        }
+        if (isOperation(char) || char == '(' || char == ')') {
+            tokens.push(char);
+        }
+    }
+    if (lastNumber.length > 0) {
+        tokens.push(lastNumber);
+    }
+    return tokens;
+}
+
+// Функция compile принимает один аргумент -- строку
+// с арифметическим выражением, записанным в инфиксной 
+// нотации, и преобразует это выражение в обратную 
+// польскую нотацию (ОПН). Возвращаемое значение -- 
+// результат преобразования в виде строки, в которой 
+// операторы и операнды отделены друг от друга пробелами. 
+// Выражение может включать действительные числа, операторы 
+// +, -, *, /, а также скобки. Все операторы бинарны и левоассоциативны.
+// Функция реализует алгоритм сортировочной станции 
+
+function compile(str) {
+    let out = [];
+    let stack = [];
+    let arr = tokenize(str)
+    for ([index, token] of arr.entries()) {
+        if (isNumeric(token)) {
+            out.push(token);
+        } else if (isOperation(token)) {
+            while (stack.length > 0 && isOperation(stack[stack.length - 1]) && priority(stack[stack.length - 1]) >= priority(token)) {
+                out.push(stack.pop());
+            }
+            stack.push(token);
+        } else if (token == '(') {
+            stack.push(token);
+        } else if (token == ')') {
+            while (stack.length > 0 && stack[stack.length - 1] != '(') {
+                out.push(stack.pop());
+            }
+            stack.pop();
+        }
+    }
+    while (stack.length > 0) {
+        out.push(stack.pop());
+    }
+    return out.join(' ');
+}
+
+// Функция evaluate принимает один аргумент -- строку 
+// с арифметическим выражением, записанным в обратной 
+// польской нотации. Возвращаемое значение -- результат 
+// вычисления выражения. Выражение может включать 
+// действительные числа и операторы +, -, *, /.
+
+function evaluate(str) {
+    const operators = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y,
+        '^': (x, y) => Math.pow(x, y)
+    };
+
+    let stack = [];
+    let arr = compile(str).split(' ')
+    arr.forEach((token) => {
+        if (token in operators) {
+            if ((token == '-' && stack.length == 1) || (token == '-' && stack.length == 2 && arr.length == 4)) stack.push(stack.pop() * -1)
+            else {
+                let [y, x] = [stack.pop(), stack.pop()];
+                stack.push(operators[token](x, y));
+                console.log(stack);
+            }
+        } else {
+            stack.push(parseFloat(token));
+            console.log(stack);
+        }
+    });
+    return stack.pop();
+}
+
+// Функция clickHandler предназначена для обработки 
+// событий клика по кнопкам калькулятора. 
+// По нажатию на кнопки с классами digit, operation и bracket
+// на экране (элемент с классом screen) должны появляться 
+// соответствующие нажатой кнопке символы.
+// По нажатию на кнопку с классом clear содержимое экрана 
+// должно очищаться.
+// По нажатию на кнопку с классом result на экране 
+// должен появиться результат вычисления введённого выражения 
+// с точностью до двух знаков после десятичного разделителя (точки).
+
+function clickHandler(event) {
+    scr.value = scr.value + event.target.textContent
+    if (event.target.textContent == '=') {
+        scr.value = evaluate(scr.value)
+    }
+    if (event.target.textContent == 'C') {
+        scr.value = ''
+    }
+}
+
+// Обработчики событий.
+window.onload = function () {
+    buttons.forEach(target => target.addEventListener('click', clickHandler))
+}
+var buttons = document.querySelectorAll('.key')
+var scr = document.querySelector('#screen')
